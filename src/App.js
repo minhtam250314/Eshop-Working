6341819e7a4bfd6203b4ce4c1d7d947406bc0164
Minh Tam <tamnhmse161193@fpt.edu.vn>
2024-11-26 17:58:56 +0700

    router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { routes } from './routes'
-import HeaderComponent from './components/HeaderComponent/HeaderComponent'
+import DefaultComponent from './components/DefaulthComponent/DefaultComponent'
 function App() {
 
   return (
     <div>
-      <HeaderComponent />
       <Router>
         <Routes>
           {routes.map((route) => {
             const Page = route.page
+            const Layout = route.isShowHeader ? DefaultComponent : Fragment
+
             return (
-              <Route path={route.path} element={<Page />} />
+              <Route key={route.path} path={route.path} element={
+                <Layout>
+                  <Page />
+                </Layout>
+              } />
             )
           })}
         </Routes>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import HomePage from '../pages/HomePage/HomePage';
+import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
 import OrderPage from '../pages/OrderPage/OrderPage';
 import ProductsPage from '../pages/ProductsPage/ProductsPage';
 
@@ -6,17 +7,20 @@ export const routes = [
     {
         path: '/',
         page: HomePage,
+        isShowHeader: true,
     },
     {
         path: '/order',
         page: OrderPage,
+        isShowHeader: true,
     },
     {
         path: '/products',
         page: ProductsPage,
+        isShowHeader: true,
     },
     {
         path: '*',
-        page: ProductsPage,
+        page: NotFoundPage,
     }
 ]
