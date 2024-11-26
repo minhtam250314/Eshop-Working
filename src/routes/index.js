014c79d6ab274a3ee1017f56fe03ffa26f29bdba
Minh Tam <tamnhmse161193@fpt.edu.vn>
2024-11-26 17:18:53 +0700

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+import HomePage from '../pages/HomePage/HomePage';
+import OrderPage from '../pages/OrderPage/OrderPage';
+import ProductsPage from '../pages/ProductsPage/ProductsPage';
+
+export const routes = [
+    {
+        path: '/',
+        page: HomePage,
+    },
+    {
+        path: '/order',
+        page: OrderPage,
+    },
+    {
+        path: '/products',
+        page: ProductsPage,
+    },
+    {
+        path: '*',
+        page: ProductsPage,
+    }
+]
