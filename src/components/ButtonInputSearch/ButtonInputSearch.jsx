b0fde8f3be5a936e9e0abebc159120c9422a5f6e
Minh Tam <tamnhmse161193@fpt.edu.vn>
2024-11-28 15:54:41 +0700

    header done

--- src/components/ButtonInputSearch/ButtonInputSearch.jsx
+++ src/components/ButtonInputSearch/ButtonInputSearch.jsx
@@ -1,26 +1,31 @@
-import { Button, Input } from "antd";
+import { Button } from "antd";
 import React from "react";
 import { SearchOutlined } from '@ant-design/icons';
+import InputComponent from "../InputComponent/InputComponent";
+import ButtonComponent from "../ButtonComponent/ButtonComponent";
 
 const ButtonInputSearch = (props) => {
-    const { size, textButton, placeholder, bordered,
+    const {
+        size, textButton, placeholder, bordered,
         backgroundColorInput = '#fff',
         backgroundColorButton = 'rgb(13, 92, 182)',
         colorButton = '#fff'
     } = props;
     return (
         <div style={{ display: 'flex' }}>
-            <Input size={size}
+            <InputComponent
+                size={size}
                 placeholder={placeholder}
                 bordered={bordered}
                 style={{ backgroundColor: backgroundColorInput }}
             />
-            <Button size={size}
-                bordered={bordered}
+            <ButtonComponent
+                size={size}
                 style={{ background: backgroundColorButton, border: !bordered && 'none' }}
-                icon={<SearchOutlined style={{ color: colorButton }} />}>
-                <span style={{ color: colorButton }}>{textButton}</span>
-            </Button>
+                icon={<SearchOutlined color={colorButton} style={{ color: '#fff' }} />}
+                textButton={textButton}
+                styleTextButton={{ color: colorButton }}
+            />
         </div>
     );
 }
