4b738faaff8402b3ec430d0c5e85a7f5a9e5c545
Minh Tam <tamnhmse161193@fpt.edu.vn>
2024-12-01 00:26:40 +0700

    slider

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import TypeProduct from '../../components/TypeProduct/TypeProduct';
 import { WrapperTypeProduct } from './style';
+import SliderComponent from '../../components/SliderComponent/SliderComponent';
+import slider1 from '../../assets/images/slider1.png';
+import slider2 from '../../assets/images/slider2.png';
+import slider3 from '../../assets/images/slider3.png';
 
 const HomePage = () => {
     const arr = ['TV', 'Tu Lanh', 'Laptop']
     return (
-        <div style={{ padding: '0 120px', fontSize: 15 }}>
-            <WrapperTypeProduct>
-                {arr.map((item) => {
-                    return (
-                        <TypeProduct name={item} key={item} />
-                    )
-                })}
-            </WrapperTypeProduct>
-            HomePage
-        </div>
+        <>
+            <div style={{ padding: '0 120px', fontSize: 15 }}>
+                <WrapperTypeProduct>
+                    {arr.map((item) => {
+                        return (
+                            <TypeProduct name={item} key={item} />
+                        )
+                    })}
+                </WrapperTypeProduct>
+            </div>
+
+            <div id="container" style={{ backgroundColor: '#efefef', padding: '0 120px' }}>
+                <SliderComponent arrImages={[slider1, slider2, slider3]} />
+            </div>
+
+        </>
     )
 }
 
--- src/pages/HomePage/style.js
+++ src/pages/HomePage/style.js
@@ -6,4 +6,5 @@ export const WrapperTypeProduct = styled.div`
   gap: 24px;
   justify-content: flex-start;
   border-bottom: 1px solid #e8e8e8;
+  height: 44px;
 `
